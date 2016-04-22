0f5b7a4fae7b1f6f5da35013d34bcbe6cb9992a5
Andrey Fedotov <protected.for@gmail.com>
2016-04-22 20:27:10 +0600

    add comments table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+         $this->call(CommentsTableSeeder::class);
 
         Model::reguard();
     }
